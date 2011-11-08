aba0b1b95abcb6e1d6b19bc026b6b3464a6252b5
Jordan Stadler <jordan.stadler@gmail.com>
2011-11-07 23:05:48 -0500

    sphere moving on random path

--- curve.c
+++ curve.c
@@ -1,61 +1,53 @@
- /*
-  * CSCI 3090U
-  * Assignment 2 - Part 2
-  * Feb. 20, 2010
-  *
-  * Jordan Stadler
-  * 100366066
-  */
- 
- #include <GL/gl.h>
- #include <GL/glu.h>
- #include <GL/glut.h>
- #include <stdio.h> 
- #include <string.h>
- #include <math.h>
- #include <time.h>
+/*
+* CSCI 3090U
+* Assignment 2 - Part 2
+* Feb. 20, 2010
+*
+* Jordan Stadler
+* 100366066
+*/
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <stdio.h> 
+#include <string.h>
+#include <math.h>
+#include <time.h>
 #include <stdlib.h>
- int window; 	//id of the window
-	double x1,x2,x3,x4,l1,y2,y3,y4;
+int window; 	//id of the window
+double x,y,u;
+double x1,x2,x3,x4,l1,y2,y3,y4;
+static double s = 0.5;
  
+void keystroke(unsigned char c, int x, int y) {
+	switch(c) {
+		case 113:
+			exit(0);
+			break;
+		case 110:
+			x1 = x2;
+			l1 = y2;
+			x2 = x3; y2 = y3;
+			x3 = x4; y3 = y4;
+			x4 = ((rand() % (99)/100.)*5)-2.5;
+			y4 = ((rand() % (99)/100.)*5)-2.5;
+			break;
+	}
+}
 void display() {
 	//printf("hello\n");
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//glPushMatrix();
+	glPushMatrix();
 	//double l1;
-	double x,y;
 	glColor3f(1.0,0.0,0.0);
 
-	//printf("%f - %f\n", x, y);
-	float t = 0.0;
-		double s = 0.5*(1-t);
-		double u;
-		//glBegin(GL_LINE_STRIP);
-		u = 0;
-		//printf("world1\n");
-		while(u<=1) {
-			//printf("world2\n");
-			x = (s * ((-1*u) + (2*pow(u,2)) + (-1*pow(u,3))) * x1) +
-				(s * (pow(u,2) + (-1 * pow(u,3))) * x2) +
-				((1 - (3*pow(u,2)) + (2*pow(u,3))) * x2) +
-				(s * (u - (2*pow(u,2)) + pow(u,3)) * x3) +
-				(((3*pow(u,2)) - (2*pow(u,3))) * x3) +
-				((s * (pow(u,3) - pow(u,2))) * x4);
-			y = (s * ((-1*u) + (2*pow(u,2)) + (-1*pow(u,3))) * l1) +
-				(s * (pow(u,2) + (-1 * pow(u,3))) * y2) +
-				((1 - (3*pow(u,2)) + (2*pow(u,3))) * y2) +
-				(s * (u - (2*pow(u,2)) + pow(u,3)) * y3) +
-				(((3*pow(u,2)) - (2*pow(u,3))) * y3) +
-				((s * (pow(u,3) - pow(u,2))) * y4);
-			//glVertex3f(x,y,0);
-			glPushMatrix();
-			glTranslatef(x,y,0);
-			glutSolidSphere(0.25,25,25);
-			glPopMatrix();
-			u+=0.1;
-		}
-		//glEnd();
+	glPushMatrix();
+	glTranslatef(x,y,0);
+	glutSolidSphere(0.25,25,25);
+	glPopMatrix();
+	
 	glColor3f(1.0,1.0,1.0);
 	glPointSize(10.0f);
 	glBegin(GL_POINTS);
@@ -68,19 +60,35 @@ void display() {
 		glVertex3f(x4, y4, 0);
 	glEnd();
 
-	//glPopMatrix();
+	glPopMatrix();
 
 	glutSwapBuffers();
 }
 
 void animate() {
-     
-     // We must set the current window, since a window isn't
-     // set before this function is called
-     glutSetWindow(window);
-     //  Ask Glut to redisplay the current window
-     glutPostRedisplay();
-     
+	if( u < 1.0) {
+		x = (s * ((-1*u) + (2*pow(u,2)) + (-1*pow(u,3))) * x1) +
+			(s * (pow(u,2) + (-1 * pow(u,3))) * x2) +
+			((1 - (3*pow(u,2)) + (2*pow(u,3))) * x2) +
+			(s * (u - (2*pow(u,2)) + pow(u,3)) * x3) +
+			(((3*pow(u,2)) - (2*pow(u,3))) * x3) +
+			((s * (pow(u,3) - pow(u,2))) * x4);
+		y = (s * ((-1*u) + (2*pow(u,2)) + (-1*pow(u,3))) * l1) +
+			(s * (pow(u,2) + (-1 * pow(u,3))) * y2) +
+			((1 - (3*pow(u,2)) + (2*pow(u,3))) * y2) +
+			(s * (u - (2*pow(u,2)) + pow(u,3)) * y3) +
+			(((3*pow(u,2)) - (2*pow(u,3))) * y3) +
+			((s * (pow(u,3) - pow(u,2))) * y4);
+		u+=0.001;
+	}else{
+		u = 0.0;
+		keystroke('n', 0, 0);
+	}
+	// We must set the current window, since a window isn't
+	// set before this function is called
+	glutSetWindow(window);
+	//  Ask Glut to redisplay the current window
+	glutPostRedisplay();
 }
      
 
@@ -132,21 +140,6 @@ void gfxinit() {
      
 }
 
-void keystroke(unsigned char c, int x, int y) {
-	switch(c) {
-		case 113:
-			exit(0);
-			break;
-		case 110:
-			x1 = x2;
-			l1 = y2;
-			x2 = x3; y2 = y3;
-			x3 = x4; y3 = y4;
-			x4 = ((rand() % (99)/100.)*5)-2.5;
-			y4 = ((rand() % (99)/100.)*5)-2.5;
-			break;
-	}
-}
 
 int main(int argc, char **argv) {
      // Initialize GLUT and process its command line arguments
