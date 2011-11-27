a94936a65c2f11e2847922478b386edcc3d543b5
Rafael Ayala <ayalarafael@gmail.com>
2011-11-26 20:47:02 -0500

    a

--- squish.c
+++ squish.c
@@ -18,45 +18,58 @@
 
 #define PI 3.14159
 
-float x1,y,z1;
-float s;
+float x1,y,z1,x2;
+float s,scalex,scaley,scalez;
+float scalex = 1.0;
+float scaley = 1.0;
+float scalez = 1.0;
 int end = 0; //boolean to make the animation oscillate
 double ini_time;
 struct timeval tiempo;
 struct timeval t_current;
 
-float left,right,top,bottom
+float left,right,top,bottom;
 static void init(void)
 {
     x1 = -10.0;
+    x2 = 50.0;
     y = 0.0;
     z1 = 0.0;
     glEnable(GL_DEPTH_TEST);
     glClearColor (0.0, 0.0, 0.0, 0.0);
     glShadeModel (GL_FLAT);
     glMatrixMode (GL_PROJECTION);
-    //gluPerspective(45.0, 1.0, 1.0,45);
-    glOrtho(-20.0,20.0,-20.0,20.0,1.0,45.0);
+    //gluPerspective(60.0, 1.0, 1.0,70);
+    glOrtho(-60.0,60.0,-60.0,60.0,0.0,80.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity ();
-    gluLookAt (1.0, 0.0, 45.0, 0.0, 0.0,0.0, 0.0, 1.0, 0.0);
+    gluLookAt (20.0, 0.0, 60.0, 20.0, 0.0,0.0, 0.0, 1.0, 0.0);
+    //srand(time(NULL));
 }
 
 void display(void)
 {
+    
+    
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glColor3f(1.0,0.0,0.0);
     
     glPushMatrix();
     glTranslatef(x1,y,z1);
+    //glScalef(scalex,scaley,scalez);
     glutSolidSphere(8,25,25);
     glPopMatrix();
     
+    glPushMatrix();
+    glTranslatef(x2,y,z1);
+    //glScalef(scalex,scaley,scalez);
+    glutSolidSphere(8,25,25);
+    glPopMatrix();
     
     glColor3f(0.0,1.0,0.0);
     glBegin(GL_LINES);
-    glVertex3f(20.0,-20.0,0.0);
-    glVertex3f(20.0,-20.0,0.0);
+    glVertex3f(20.0,-15.0,0.0);
+    glVertex3f(20.0,15.0,0.0);
     glEnd();
     
     glutSwapBuffers();
@@ -68,13 +81,19 @@ void animate(void)
         //printf("HUUURRR\n");
         //Start easing in 
         
-        
-        //glutIdleFunc(NULL);
+        glutIdleFunc(NULL);
     } else {
+        if(scalex > 0){
+            scalex-=0.0001;
+            scaley-=0.0001;
+            scalez-=0.0001;
+        }
         x1 += 0.01;
+        x2 -= 0.01;
         glutPostRedisplay();
     }
-    //printf("%f\n",x1);
+    //printf(%);
+    printf("%f\n",x2);
         
     
 }
